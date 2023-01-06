b05b2327d33cc0497872c469595437ab9dbd5284
Mohit-k-Mummon <mxkumar@protonmail.com>
2023-01-06 00:36:41 -0800

    JS not finished + added parcel for building

---
+++ app.js
@@ -0,0 +1,43 @@
+'strict';
+
+// Variables
+
+// Form
+const form = document.getElementById('form');
+
+// Inputs
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const messageInput = document.getElementById('message-input');
+
+// Button
+const submitButton = document.getElementById('submit-btn');
+
+nameInput.addEventListener('input', event => {
+	let target = event.target.value;
+
+	if (target.length > 0) {
+		nameInput.style.borderBottom = '1px solid #4ee1a0';
+		isValid = true;
+	} else {
+		nameInput.style.borderBottom = '1px solid red';
+		isValid = false;
+	}
+});
+
+function validate(event) {
+	// State
+	let isValid = false;
+
+	// Prevent Default
+	event.preventDefault();
+}
+
+// Regex function
+function nameValidate(input) {
+	return /^[A-Z][a-z]+\s[A-Z][a-z]+$/gi.test(input);
+}
+
+function isEmail(input) {
+	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
+}
